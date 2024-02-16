773705c030907223b96aa0127f446a5b9074e8d6
Julian <julian_p@snu.ac.kr>
2024-02-16 23:09:16 +0900

    Fix run_random.py

--- src/rl_ai2thor/envs/run_random.py
+++ src/rl_ai2thor/envs/run_random.py
@@ -12,8 +12,10 @@ from rl_ai2thor.utils.general_utils import ROOT_DIR
 NB_STEPS = 1000
 
 OUTPUT_DIR = ROOT_DIR / "outputs" / "random_agent"
-output_idx = len(os.listdir(OUTPUT_DIR))
-current_output_dir = Path(OUTPUT_DIR)
+OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+
+run_idx = len(os.listdir(OUTPUT_DIR))
+current_output_dir = OUTPUT_DIR / f"run_{run_idx}"
 current_output_dir.mkdir(parents=True, exist_ok=True)
 
 video_writer = imageio.get_writer(current_output_dir / "video.mp4", fps=10)
